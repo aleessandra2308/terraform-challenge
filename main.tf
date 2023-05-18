dbc4a94e59c8cdf1e66baa626b0f4449ae2c207a
Alessandra Rosado Villalobos <alerosado@MacBook-Pro-de-Aleee.local>
2023-05-18 00:55:54 -0500

    1.Create resource group

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
-resource "random_pet" "rg_name" {
-    prefix = var.resource_group_name_prefix
-}
-
-resource "azurerm_resource_group" "rg" {
-    location = var.resource_group_location
-    name     = random_pet.rg_name.id
+//1. Creating resource group
+resource "azurerm_resource_group" "challengue" {
+  name = "rg-challenge"
+  location = var.resource_group_location
 }
